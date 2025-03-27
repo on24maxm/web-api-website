e21b4161dfe9120bcc935a50d9c25bcd9e3b89b6
on24maxm <max.malimonenko.24@lehre.mosbach.dhbw.de>
2025-03-27 17:41:05 +0100

    codebeispiele hinzugefügt

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-const beispiel1 = document.querySelector("#beispiel1");
-beispiel1.addEventListener("click", executeExample);
+//Grundlegendes Beispiel
 
-function executeExample() {
-  alert("das ist ein Beispiel für den Funtionsaufruf");
+const beispiel_grundlegend = document.getElementById("beispiel_grundlegend");
+
+class Tier {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sprich() {
+    alert(this.name + " macht ein Geräusch.");
+  }
 }
+
+const hund = new Tier("Bello");
+
+beispiel_grundlegend.addEventListener("click", function() {
+  hund.sprich();
+});
+
+//Vererbung
+
+const beispiel_vererbung = document.getElementById("beispiel_vererbung");
+
+class Hund extends Tier {
+  constructor(name, rasse) {
+    super(name); // Ruft den Konstruktor von Tier auf
+    this.rasse = rasse;
+  }
+
+  sprich() {
+    alert("Wuff!");
+  }
+
+  zeigeRasse() {
+    console.log(`Dieser Hund ist ein ${this.rasse}.`);
+  }
+}
+
+const meinHund = new Hund("Blake", "Golden Retriever");
+
+beispiel_vererbung.addEventListener("click", function() {
+  meinHund.sprich();
+  meinHund.zeigeRasse();
+});
